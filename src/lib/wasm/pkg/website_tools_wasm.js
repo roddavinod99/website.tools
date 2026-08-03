98a8c169c401aa39e0cd914c532f79da07c49b38
roddavinod99 <roddavinod99@gmail.com>
2026-08-03 18:24:54 +0530

    changes done 0.2.3

--- src/lib/wasm/pkg/website_tools_wasm.js
+++
@@ -1,9 +0,0 @@
-/* @ts-self-types="./website_tools_wasm.d.ts" */
-import * as wasm from "./website_tools_wasm_bg.wasm";
-import { __wbg_set_wasm } from "./website_tools_wasm_bg.js";
-
-__wbg_set_wasm(wasm);
-wasm.__wbindgen_start();
-export {
-    base64_decode, base64_encode, base64_url_decode, base64_url_encode, hmac_sha1, hmac_sha256, hmac_sha512, json_format, json_minify, json_validate, md5_hash, sha1_hash, sha224_hash, sha256_hash, sha384_hash, sha512_hash, url_decode, url_encode
-} from "./website_tools_wasm_bg.js";
